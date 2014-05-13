cc990ffedd4697cc74ed4d00b16b5cdc03e29609
Qing Chang <connor@petabi.com>
2014-05-13 15:25:37 -0700

    Rename the script to more descriptive name

---
+++ make_install_nic_driver_w_netmap.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+make_install_nic_driver_w_netmap() 
+{
+    echo "make install $1 with netmap support"
+    local driver="$1"
+    cd sys/modules/$driver
+    make clean && make -DDEV_NETMAP && make install
+}
+
+make_install_nic_driver_w_netmap "ixgbe"
+make_install_nic_driver_w_netmap "igb"
+
+
+
