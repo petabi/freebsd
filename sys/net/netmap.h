ff3cda65c6175bbab5bc41caab753cc21682d464
Jungkeun Park <parkjk@konkuk.ac.kr>
2014-11-14 18:30:11 +0900

    Add comment for the new flags in the netmap slot

--- sys/net/netmap.h
+++ sys/net/netmap.h
@@ -202,6 +202,7 @@ struct netmap_slot {
 
 #define NS_OFFLOAD_CSUM 0x0040
 #define NS_OFFLOAD_CTX  0x0080
+	/* Petabi: for setting checksum offloading context */
 
 #define	NS_PORT_SHIFT	8
 #define	NS_PORT_MASK	(0xff << NS_PORT_SHIFT)
