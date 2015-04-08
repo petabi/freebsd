a0f6ec76a02d80f66189811b27d5ef7a468fd971
Qing Chang <connor@petabi.com>
2015-04-07 17:02:53 -0700

    Clean up code

--- sys/dev/netmap/if_em_netmap.h
+++ sys/dev/netmap/if_em_netmap.h
@@ -161,80 +161,50 @@ em_netmap_txsync(struct netmap_kring *kring, int flags)
                         if (slot->flags & 0x0080) {
                                 struct e1000_context_desc *TXD;
                                 int ip_off, ip_hlen = 0, hdr_len = 0;
-                                /* u16 offload = 0; */
                                 u8 tucso = 0, tucss = 0, ipcss = 0, ipcso = 0;
-				u32 cmd = 0;
+                                u32 cmd = 0;
 
                                 ip_off = ETHER_HDR_LEN;
                                 ip_hlen = slot->ptr & 0x7f;
-				hdr_len = ip_off + ip_hlen;
+                                hdr_len = ip_off + ip_hlen;
 
-				txd_upper |= E1000_TXD_POPTS_IXSM << 8;
-				/* offload |= CSUM_IP; */
-				ipcss = ip_off;
-				ipcso = ip_off + offsetof(struct ip, ip_sum);
-				/*
-				 * Start offset for header checksum calculation.
-				 * End offset for header checksum calculation.
-				 * Offset of place to put the checksum.
-				 */
-				TXD = (struct e1000_context_desc *)curr;
-				TXD->lower_setup.ip_fields.ipcss = ipcss;
-				TXD->lower_setup.ip_fields.ipcse = htole16(hdr_len);
-				TXD->lower_setup.ip_fields.ipcso = ipcso;
-				cmd |= E1000_TXD_CMD_IP;
+                                ipcss = ip_off;
+                                ipcso = ip_off + offsetof(struct ip, ip_sum);
+                                /*
+                                 * Start offset for header checksum calculation.
+                                 * End offset for header checksum calculation.
+                                 * Offset of place to put the checksum.
+                                 */
+                                TXD = (struct e1000_context_desc *)curr;
+                                TXD->lower_setup.ip_fields.ipcss = ipcss;
+                                TXD->lower_setup.ip_fields.ipcse = htole16(hdr_len);
+                                TXD->lower_setup.ip_fields.ipcso = ipcso;
+                                cmd |= E1000_TXD_CMD_IP;
 
+                                tucss = hdr_len;
                                 if (slot->ptr & 0x80) {
-				  tucss = hdr_len;
-				  /* if (slot->flags & 0x0080) { */
-				    /* offload |= 0x80; */
-				  tucso = hdr_len + offsetof(struct tcphdr, th_sum);
-				} else {
-				    /* offload |= 0x40; */
-				  tucso = hdr_len + offsetof(struct udphdr, uh_sum);
-				}
-				  /*
-				   * Setting up new checksum offload context for every frames
-				   * takes a lot of processing time for hardware. This also
-				   * reduces performance a lot for small sized frames so avoid
-				   * it if driver can use previously configured checksum
-				   * offload context.
-				   */
-				  /* if (txr->last_hw_offload == offload) { */
-				  /*   if (txr->last_hw_ipcss != ipcss || */
-				  /* 	txr->last_hw_ipcso != ipcso || */
-				  /* 	txr->last_hw_tucss != tucss || */
-				  /* 	txr->last_hw_tucso != tucso) { */
-				  /*     txr->last_hw_offload = offload; */
-				  /*     txr->last_hw_tucss = tucss; */
-				  /*     txr->last_hw_tucso = tucso; */
+                                        tucso = hdr_len + offsetof(struct tcphdr, th_sum);
+                                } else {
+                                        tucso = hdr_len + offsetof(struct udphdr, uh_sum);
+                                }
 
-				      /*
-				       * Start offset for payload checksum calculation.
-				       * End offset for payload checksum calculation.
-				       * Offset of place to put the checksum.
-				       */
-				      /* TXD = (struct e1000_context_desc *)curr; */
-				TXD->upper_setup.tcp_fields.tucss = hdr_len;
-				TXD->upper_setup.tcp_fields.tucse = htole16(0);
-				TXD->upper_setup.tcp_fields.tucso = tucso;
-				cmd |= E1000_TXD_CMD_TCP;
+                                TXD->upper_setup.tcp_fields.tucss = hdr_len;
+                                TXD->upper_setup.tcp_fields.tucse = htole16(0);
+                                TXD->upper_setup.tcp_fields.tucso = tucso;
+                                cmd |= E1000_TXD_CMD_TCP;
+                                TXD->tcp_seg_setup.data = htole32(0);
+                                TXD->cmd_and_length =
+                                     htole32(adapter->txd_cmd | E1000_TXD_CMD_DEXT | cmd);
+                                nm_i = nm_next(nm_i, lim);
+                                nic_i = nm_next(nic_i, lim);
+                                continue;
+                        }
 
-				/* txr->last_hw_ipcss = ipcss; */
-				/* txr->last_hw_ipcso = ipcso; */
-				TXD->tcp_seg_setup.data = htole32(0);
-				  TXD->cmd_and_length =
-				    htole32(adapter->txd_cmd | E1000_TXD_CMD_DEXT | cmd);
-				  nm_i = nm_next(nm_i, lim);
-				  nic_i = nm_next(nic_i, lim);
-				  continue;
-			}
-
-			if (slot->flags & 0x0040) {
-				  txd_lower = E1000_TXD_CMD_DEXT | E1000_TXD_DTYP_D;
-				  txd_upper |= E1000_TXD_POPTS_TXSM << 8;
-			}
-
+                        if (slot->flags & 0x0040) {
+                                txd_lower = E1000_TXD_CMD_DEXT | E1000_TXD_DTYP_D;
+                                txd_upper |= E1000_TXD_POPTS_TXSM << 8;
+                                txd_upper |= E1000_TXD_POPTS_IXSM << 8;
+                        }
 
 			if (slot->flags & NS_BUF_CHANGED) {
 				curr->buffer_addr = htole64(paddr);
