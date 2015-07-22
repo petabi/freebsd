f5c7304e15d6d7b08df155b67db81cd02c0ed0c2
Qing Chang <connor@petabi.com>
2015-07-22 22:19:05 +0000

    Fix a typo

--- sys/dev/ixgbe/if_ix.c
+++ sys/dev/ixgbe/if_ix.c
@@ -271,7 +271,7 @@ static SYSCTL_NODE(_hw, OID_AUTO, ix, CTLFLAG_RD, 0,
 */
 /*
 ** Petabi turn off enable_aim by default, this solves pius slow down
-** issue by too many interrpts
+** issue by too many interrupts
 */
 static int ixgbe_enable_aim = FALSE;
 TUNABLE_INT("hw.ix.enable_aim", &ixgbe_enable_aim);
